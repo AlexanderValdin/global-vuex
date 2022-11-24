15f9f87db9a48acf236c19a78547d1dd74580613
Alexander Valenzuela <alvalenzuela2304@gmail.com>
2022-11-24 13:16:57 -0300

    estructura vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,24 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
+  state: { // Data 
+          // aquí se ubican los distintos estados del store,
+          // haciendo una comparativa con el Option API, sería el equivalente al data()
+  },      // aquí van todos los datos que contiene el Store, las distintas variables que tendremos
+  getters: { // Computed 
+            // métodos que nos permiten acceder a la información de un estado,
+            // nos permiten acceder a los elementos que tendremos en el state
   },
-  getters: {
+  mutations: { // Set sync 
+              // los métodos equivalentes a un setter,
+              // son métodos que nos permiten modificar los estados
   },
-  mutations: {
+  actions: { // Set async  
+            // las actions al igual que las mutations,
+            // nos permiten modificar los estados de los state,
+            // pero con la diferencia que las actions son modificaciones async
   },
-  actions: {
-  },
-  modules: {
+  modules: { // subdivide the store
+            // nos permiten subdividir nuestro store y hacer una estructuración del mismo a través de sub-elementos
   }
 })
